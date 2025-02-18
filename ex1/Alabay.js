318854304d18dd8d0a996ebeb82a8510cbf649ec
AlexLi727 <alexavililee@gmail.com>
2025-02-18 19:28:51 +0100

    madre mia willy

---
+++ ex1/Alabay.js
@@ -0,0 +1,3 @@
+let form1 = document.forms["form1"];
+let edadcheck = form1["edadcheck"];
+let edadfecha = form1["edadfecha"];
---
+++ ex2/Alabay.js
@@ -0,0 +1,3 @@
+let formulario = document.forms["formulario"];
+let texto = formulario["texto"];
+let email = formulario["email"];
